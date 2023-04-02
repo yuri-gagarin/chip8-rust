ae26ed50da9744adc7e4d49aba7f989c676d94b7
Yuriy Ivanov <ymivanov@icloud.com>
2023-04-02 07:57:21 -0400

    sketch up for mem and chip8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{fs::File, io::Read};
+
+mod ram;
+mod chip8;
+
+use chip8::{Chip8};
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("data/INVADERS").unwrap();
+    let mut data = Vec::<u8>::new();
+    file.read_to_end(&mut data).expect("DATA READ ERROR");
+
+    print!("DATA IS {:?}", data);
+
+    // load //
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(&data);
+
 }
---
+++ src/ram.rs
@@ -0,0 +1,17 @@
+pub struct Ram {
+    memory: [u8; 4096],
+}
+
+impl Ram {
+    pub fn new() -> Ram {
+        Ram {
+            memory: [0; 4096],
+        }
+    }
+    pub fn write_byte(&mut self, mem_address: u16, value: u8) {
+        self.memory[mem_address as usize] = value;
+    }
+    pub fn read_byte(&mut self, mem_address: u16, value: u8) {
+
+    }
+}
